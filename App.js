6d55f3ce655178c7d28c94d09942455c77075235
damionjerrod <damion.dawson@gmail.com>
2020-06-24 14:54:59 -0400

    Functional Shopping List App

---
+++ App.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
+
+const App = () => {
+  const [items, setItems] = useState([
+    {id: Math.random().toString(), text: 'Milk'},
+    {id: Math.random().toString(), text: 'Eggs'},
+    {id: Math.random().toString(), text: 'Bread'},
+    {id: Math.random().toString(), text: 'Juice'},
+  ]);
+
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
+
+  const addItem = text => {
+    !text
+      ? Alert.alert('Item Field Blank', 'PLease enter an item', {text: 'Ok'})
+      : setItems(prevItems => {
+          return [{id: Math.random().toString(), text}, ...prevItems];
+        });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header title="Shopping List" />
+      <AddItem addItem={addItem} />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60,
+  },
+});
+
+export default App;
